41f03fcf6cbd6a462bd0477f9dea306370b0c728
Katrina.Tomaszczyk <katrina.tomaszczyk@ladbrokes.com.au>
2023-07-24 19:37:58 +1000

    added base components badge button card

--- src/components/ui/BaseBadge.vue
+++ src/components/ui/BaseBadge.vue
@@ -1,8 +1,19 @@
 <template>
-    <div>
-
-    </div>
+  <span class="badge" :class="type">
+    {{ text }}
+  </span>
 </template>
+
+<script>
+export default {
+  props: ['type', 'title'],
+  computed: {
+    text() {
+      return this.title.toUpperCase();
+    }
+  }
+}
+</script>
 
 <style scoped>
 .badge {
