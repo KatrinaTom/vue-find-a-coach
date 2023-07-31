3b332a337191e010c174056574a0ee513eddd25b
Katrina.Tomaszczyk <katrina.tomaszczyk@ladbrokes.com.au>
2023-07-31 16:35:08 +1000

    fix on page names

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistation from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
@@ -16,14 +16,14 @@ const router = createRouter({
     { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: CoachDetail,
+      component: CoachDetails,
       props: true,
       children: [
         { path: 'contact', component: ContactCoach } // /coaches/c1/contact
       ]
     },
     { path: '/register', component: CoachRegistation, meta: { requiresAuth: true } },
-    { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+    { path: '/requests', component: RequestReceived, meta: { requiresAuth: true } },
     { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
     { path: '/:notFound(.*)', component: NotFound }
   ]
